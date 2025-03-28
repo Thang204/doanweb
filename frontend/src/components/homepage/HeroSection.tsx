0aeea3e85438d7d9e43921869a4041358d15cfa9
Ryota315 <cclone2021@gmail.com>
2025-03-28 19:33:04 +0700

    frontend

--- frontend/src/components/homepage/HeroSection.tsx
+++ frontend/src/components/homepage/HeroSection.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <img 
-          src="https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1950&q=80" 
+          src="https://images.unsplash.com/photo-1695686331766-16a2c3c2c382?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
           alt="Hero background" 
           className="object-cover w-full h-full opacity-90"
         />
